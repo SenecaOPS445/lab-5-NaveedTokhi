ede60d89375308c7672393e5c6c87367a609019b
Naveed Tokhi <dmuhammad4@myseneca.ca>
2025-02-13 20:21:38 -0500

    Lab 5 Completed

--- lab5a.py
+++ lab5a.py
@@ -3,15 +3,15 @@
 
 def read_file_string(file_name):
     # Takes file_name as string for a file name, returns its entire contents as a string
-    with open(file_name, 'r') as file:
-        content = file.read()
+    with open(file_name, 'r') as file: # open file in read mode
+        content = file.read() # read the file contents
         file.close()
     return content
 def read_file_list(file_name):
     # Takes a file_name as string for a file name, 
     # return its entire contents as a list of lines without new-line characters
-    with open(file_name, 'r') as file:
-        lines = file.read().splitlines()
+    with open(file_name, 'r') as file: # open file in read mode
+        lines = file.read().splitlines() # read the file and split the lines
         file.close()
     return lines
 if __name__ == '__main__':
